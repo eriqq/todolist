b7d91734fe3ba8ef92455c2d33910ed3dcab6b34
eriqq <erik.bjerkfeldt@gmail.com>
2018-12-01 15:56:59 +0100

    Updates

---
+++ precache-manifest.b059a3d9f7d0795640abc8c16bf11658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "7f20249f7589781f31e3",
+    "url": "/todolist/static/js/main.7f20249f.chunk.js"
+  },
+  {
+    "revision": "d715c167ec748181a577",
+    "url": "/todolist/static/js/1.d715c167.chunk.js"
+  },
+  {
+    "revision": "7f20249f7589781f31e3",
+    "url": "/todolist/static/css/main.6b17b318.chunk.css"
+  },
+  {
+    "revision": "48e4c6f8d228a44b18dfa0545e15794b",
+    "url": "/todolist/index.html"
+  }
+];
